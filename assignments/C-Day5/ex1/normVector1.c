302cb33f17dc640c1e824de43bebeef3ed87fa47
William Wang <williamq.wang@mail.utoronto.ca>
2023-06-09 10:24:12 -0700

    Add files via upload

--- assignments/C-Day5/ex1/normVector1.c
+++ assignments/C-Day5/ex1/normVector1.c
@@ -5,7 +5,7 @@
 #include <omp.h>
 
 int main(int argc, char **argv) {
-  int nThreads = 0;
+  /i/nt nThreads = 0;
   double norm = 0;
   // create vector with random numbers
   if (argc != 2) {
@@ -13,7 +13,8 @@ int main(int argc, char **argv) {
     return 0;
   }
   int vectorSize = atoi(argv[1]);
-  double *globalVector = malloc(sizeof *globalVector * vectorSize);
+  //double *globalVector = malloc(sizeof *globalVector * vectorSize);
+  double globalVector[vectorSize];
   srand(50);
   for (int i = 0; i < vectorSize; i++) {
     double random_number = 1.0 + (double)rand() / RAND_MAX;
@@ -24,25 +25,19 @@ int main(int argc, char **argv) {
   double sum[64][16]; //assume max 64 threads, pad at 16. only use [i][0]
   for (int i=0; i<64; i++){sum[i][0] = 0;} // initialize to 0
 
-  #pragma omp parallel
+  #pragma omp parallel reduction(+:norm) //only 1 thread will do plus operators on 'dot'
   {
     int tid = omp_get_thread_num(); //id of the thread
     int numT = omp_get_num_threads(); //total number of threads
-    if (tid == 0) {nThreads = numT;} //only master sets this global variable
-    // dot product
-    for (int i=0; i<vectorSize; i++) {
-      sum[i][0] += globalVector[i]*globalVector[i];
-    }
+    #pragma omp parallel for
+      for (int i =tid; i < vectorSize; i+= numT){
+        norm += globalVector[i]*globalVector[i];
+      }
   }
 
-  // add back
-  for (int i = 0; i < nThreads; i++){
-    norm += sum[i][0];
-  }
   norm = sqrt(norm);
-
   // print norm
   printf("Norm = %f \n", norm);
-  free(globalVector);
+  //free(globalVector);
   return 0;
 }
